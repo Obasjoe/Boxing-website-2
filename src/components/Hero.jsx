5994ce8ffbf4c34f3624421c972a41596dbe1ca2
Prince Niyonkuru <pniyonku@gmail.com>
2024-01-19 14:05:56 -0600

    Working only on hero section without Navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Css/Hero.css'
+const Hero = () => {
+    return (
+        < >
+            <div className="hero_container">
+                <p>Your text goes here</p>
+            </div>
+        </>
+    )
+    }
+    export default Hero
